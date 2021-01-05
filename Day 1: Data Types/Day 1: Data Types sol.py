b42fb436eb50ed879a1b018f8706f361b12de181
engalgamal96 <engalgamal96@gmail.com>
2021-01-05 04:37:02 +0200

    Initial commit

---
+++ Day 1: Data Types/Day 1: Data Types sol.py
@@ -0,0 +1,12 @@
+def data_type(i,d,s):
+    i+=4
+    d+=4.0
+    s='HackerRank '+s
+    print(i)
+    print(d)
+    print(s)
+if __name__=='__main__':
+    i=int(input())
+    d=float(input())
+    s=str(input())
+    data_type(i,d,s)
