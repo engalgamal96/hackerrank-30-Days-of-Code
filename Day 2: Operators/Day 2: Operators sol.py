2ad7c364ef5d62b0a20a26e68921aa83aa934d56
engalgamal96 <engalgamal96@gmail.com>
2021-01-05 05:24:57 +0200

    Initial commit

--- Day 2: Operators/Day 2: Operators sol.py
+++ Day 2: Operators/Day 2: Operators sol.py
@@ -3,12 +3,8 @@ def solve(meal_cost, tip_percent, tax_percent):
     tax=meal_cost*tax_percent/100
     male_price=meal_cost+tip+tax
     print(str(round(male_price)))
-
 if __name__ == '__main__':
     meal_cost = float(input())
-
     tip_percent = int(input())
-
     tax_percent = int(input())
-
     solve(meal_cost, tip_percent, tax_percent)
